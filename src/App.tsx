3a6817a08b5c69db35244c65a0ff11652de54b5b
nacz0 <natmach2004@gmail.com>
2023-09-22 15:48:27 +0200

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Boxshadow } from "./components/BoxShadow";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { TextShadow } from "./components/TextShadow";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <TextShadow />,
+  },
+]);
 function App() {
   return (
     <main className="w-screen h-screen flex flex-col bg-slate-200">
-      <Boxshadow />
+      <RouterProvider router={router} />
     </main>
   );
 }
