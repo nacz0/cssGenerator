5a9ada8a6393133199988bfcc0370057d9b276c6
nacz0 <natmach2004@gmail.com>
2023-09-22 19:33:18 +0200

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Boxshadow } from "./components/BoxShadow";
+import { Root } from "./components/Root";
 import { TextShadow } from "./components/TextShadow";
+import { TextStroke } from "./components/TextStroke";
+import { Gradient } from "./components/Gradient";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <TextShadow />,
+    element: <Root />,
+    children: [
+      {
+        path: "text-shadow",
+        element: <TextShadow />,
+      },
+      {
+        path: "box-shadow",
+        element: <Boxshadow />,
+      },
+      {
+        path: "text-stroke",
+        element: <TextStroke />,
+      },
+      {
+        path: "gradient",
+        element: <Gradient />,
+      },
+    ],
   },
 ]);
 function App() {
-  return (
-    <main className="w-screen h-screen flex flex-col bg-slate-200">
-      <RouterProvider router={router} />
-    </main>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
